1a8bdff9566edcbf332d902307d7b79652582954
MichaelScofield45 <paco.villamonte@gmail.com>
2023-04-20 03:59:36 +0200

    Initial commit, basic building config for system and home-manager

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        ms45 = lib.nixosSystem {
+          inherit system;
+          modules = [ 
+           ./configuration.nix 
+           home-manager.nixosModules.home-manager {
+             home-manager.useGlobalPkgs = true;
+             home-manager.useUserPackages = true;
+             home-manager.users.ms45 = {
+               imports = [ ./home.nix ];
+             };
+           }
+          ];
+        };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "ms45";
+  home.homeDirectory = "/home/ms45";
+  home.stateVersion = "22.11";
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    htop
+    neovim
+    ffmpeg
+    nnn
+    git
+  ];
+}
