e3d2a3906ee2595faab8e9076cc8a97c91ca70d7
MichaelScofield45 <paco.villamonte@gmail.com>
2024-03-26 19:39:41 -0600

    common: desktopManager option is now under services

--- hosts/common.nix
+++ hosts/common.nix
@@ -26,7 +26,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     elisa
     kate
