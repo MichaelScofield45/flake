15d7ce666eefe90641d1c17c29ba6916509b0090
MichaelScofield45 <paco.villamonte@gmail.com>
2025-01-24 19:06:46 -0600

    Gcc needed to compile treesitter grammars in neovim, it's also neat to have in system

--- hosts/common.nix
+++ hosts/common.nix
@@ -37,6 +37,7 @@
 
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    gcc
   ];
 
   # Enable the OpenSSH daemon.
