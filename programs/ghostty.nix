3e1801e2e3bcc3b9165ba55eed2ecc9647e10647
MichaelScofield45 <paco.villamonte@gmail.com>
2025-02-03 23:16:47 -0600

    Add working blur, update vim split keybinds to new definitions
    Blur is controlled by KWin and is independent from Ghostty, only KDE
    supports blur until now.

--- programs/ghostty.nix
+++ programs/ghostty.nix
@@ -14,11 +14,11 @@
       window-padding-x = 8;
       window-decoration = false;
       background-opacity = 0.7;
-      background-blur-radius = 50;
+      background-blur = true;
       keybind = [
         "ctrl+alt+h=goto_split:left"
-        "ctrl+alt+j=goto_split:bottom"
-        "ctrl+alt+k=goto_split:top"
+        "ctrl+alt+j=goto_split:down"
+        "ctrl+alt+k=goto_split:up"
         "ctrl+alt+l=goto_split:right"
       ];
     };
