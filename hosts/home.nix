cb3b46c1dd6b461f8c16e718680a31332409a070
MichaelScofield45 <paco.villamonte@gmail.com>
2025-02-16 21:18:43 -0600

    Conditional merge of attributes dependeng on headless option

--- hosts/home.nix
+++ hosts/home.nix
@@ -12,102 +12,115 @@
     ../programs/fish.nix
     ../modules/hm/headless.nix
   ];
+  config = lib.mkMerge [
+    ({
+      home.username = "ms45";
+      home.homeDirectory = "/home/ms45";
+      home.stateVersion = "22.11";
 
-  home.username = "ms45";
-  home.homeDirectory = "/home/ms45";
-  home.stateVersion = "22.11";
+      programs.home-manager.enable = true;
 
-  programs.home-manager.enable = true;
+      home.packages = with pkgs; [
+        # Terminal related pkgs
+        (python311.withPackages (ps: with ps;[
+          numpy
+          ipython
+          matplotlib
+          pandas
+          pynvim
+          jupyter-client
+        ]))
+        bat
+        fd
+        ffmpeg
+        file
+        gdb
+        git
+        htop
+        libertine
+        jetbrains-mono
+        iosevka-bin
+        lazygit
+        less
+        ripgrep
+        tealdeer
+        trash-cli
+        luajit
+        luajitPackages.luarocks
+        unzip
+        p7zip
+        gnumake
+        neovim
+        taskwarrior3
+        typst
+        yazi
+      ];
 
-  home.packages = with pkgs; [
-    # Terminal related pkgs
-    (python311.withPackages (ps: with ps;[
-      numpy
-      ipython
-      matplotlib
-      pandas
-      pynvim
-      jupyter-client
-    ]))
-    bat
-    fd
-    ffmpeg
-    file
-    fzf
-    gdb
-    git
-    htop
-    libertine
-    jetbrains-mono
-    iosevka-bin
-    lazygit
-    less
-    nnn
-    ripgrep
-    tealdeer
-    trash-cli
-    luajit
-    luajitPackages.luarocks
-    unzip
-    p7zip
-    gnumake
-    neovim
-    typst
-    yazi
-  ] ++ lib.optionals (!config.headless.enable) (with pkgs; [
-    # GUI apps
-    foliate
-    chromium
-    calibre
-    firefox-beta
-    inkscape
-    krita
-    lutris
-    pavucontrol
-    obs-studio
-    qbittorrent
-    qalculate-qt
-    rnote
-    vscodium
-    wineWowPackages.staging
-    wl-clipboard
-    xclip
-    xournalpp
-  ]);
+      programs.zoxide.enable = true;
+      programs.fzf.enable = true;
+      programs.nnn.enable = true;
 
-  programs.zoxide.enable = true;
+      home.sessionVariables = {
+        EDITOR = "nvim";
+        PAGER = "less";
+      };
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    PAGER = "less";
-  };
+      home.sessionPath = [
+        "$HOME/.local/bin"
+      ];
 
-  home.sessionPath = [
-    "$HOME/.local/bin"
+      programs.bash.enable = true;
+      programs.btop = {
+        enable = true;
+        settings = {
+          color_theme = "tokyo-night";
+          vim_keys = true;
+        };
+      };
+    })
+
+
+    (lib.mkIf (!config.headless.enable) {
+      home.packages = with pkgs; [
+        # GUI apps
+        foliate
+        chromium
+        calibre
+        firefox-beta
+        inkscape
+        krita
+        lutris
+        pavucontrol
+        obs-studio
+        qbittorrent
+        qalculate-qt
+        rnote
+        vscodium
+        wineWowPackages.staging
+        wl-clipboard
+        xclip
+        xournalpp
+      ];
+
+      mine.kitty.enable = true;
+      mine.ghostty.enable = true;
+      mine.blender.enable = true;
+
+      services.syncthing.enable = true;
+      fonts.fontconfig.enable = true;
+
+      programs.mpv = {
+        enable = true;
+        config = {
+          hwdec = "auto";
+          vo = "gpu";
+        };
+      };
+
+      services.easyeffects = {
+        enable = true;
+        preset = "Default";
+      };
+    })
   ];
-
-  programs.bash.enable = true;
-  programs.btop = {
-    enable = true;
-    settings = {
-      color_theme = "tokyo-night";
-      vim_keys = true;
-    };
-  };
-
-  services.syncthing.enable = true;
-  fonts.fontconfig.enable = true;
-
-  programs.mpv = {
-    enable = true;
-    config = {
-      hwdec = "auto";
-      vo = "gpu";
-    };
-  };
-
-  services.easyeffects = {
-    enable = true;
-    preset = "Default";
-  };
 }
